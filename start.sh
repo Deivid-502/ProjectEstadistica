89d49ae14f00867e8d957c9c9744f7be5a6332c1
David Hernandez <davidhernandez502jc@gmail.com>
2025-10-23 14:25:14 -0600

    Version para Deployment - Arreglos para RailWay

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-PORT=${PORT:-8501}
+# Use PORT provisto por Railway. No usar valor por defecto que no coincida.
 echo "Iniciando Streamlit en 0.0.0.0:${PORT}"
 streamlit run covid_stats_app/app.py --server.port ${PORT} --server.address 0.0.0.0
