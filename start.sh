dbd749ead4a33ff7ea5197c2edf73810df3edd05
David Hernandez <davidhernandez502jc@gmail.com>
2025-10-23 15:50:49 -0600

    Version para Deployment - Arreglos para RailWay

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-# Use PORT provisto por Railway. No usar valor por defecto que no coincida.
+# Asegurar que el directorio raíz del repo esté en PYTHONPATH
+export PYTHONPATH=$(pwd)
+
+# Use PORT provisto por Railway.
 echo "Iniciando Streamlit en 0.0.0.0:${PORT}"
 streamlit run covid_stats_app/app.py --server.port ${PORT} --server.address 0.0.0.0
